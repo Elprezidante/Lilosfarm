29274d903289a4251a6ac5b671aaff1d23074d5b
MODCOM INSTITUTE <isaacm2540@gmail.com  git config --global user.name Elprezidanteecho>
2026-05-09 18:25:41 +0300

    commited new ones

--- src/components/Getproducts.jsx
+++ src/components/Getproducts.jsx
@@ -178,7 +178,7 @@ const Getproducts = () => {
     prev.map(i => i._key === key ? { ...i, quantity: i.quantity + delta } : i).filter(i => i.quantity > 0)
   );
   const removeItem = (key) => setCart(prev => prev.filter(i => i._key !== key));
-  const _clearCart = () => setCart([]);
+ 
 
   const handleCheckout = () => {
     if (!cart.length) return;
